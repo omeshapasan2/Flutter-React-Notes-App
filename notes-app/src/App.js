2170fc907d1d48f4fadeac3b9cf2f9437e870921
omeshapasan2 <omesha.20232170@iit.ac.lk>
2025-03-26 22:47:05 +0530

    Note App Basics Done

--- notes-app/src/App.js
+++ notes-app/src/App.js
@@ -1,38 +1,69 @@
 import NotesList from "./components/NotesList";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { nanoid } from "nanoid";
+import Search from "./components/Search";
+import Header from "./components/Header";
 
 const App = () => {
-  const [notes, setNotes] = useState([
-    {
+  const [notes, setNotes] = useState(() => {
+    const savedNotes = localStorage.getItem('react-notes-app-data');
+    return savedNotes ? JSON.parse(savedNotes) : [];
+  });
+
+  const [searchText, setSearchText] = useState('');
+
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    const savedNotes = JSON.parse(
+        localStorage.getItem('react-notes-app-data')
+    );
+
+    if (savedNotes) {
+        // Use saved notes instead of initial hardcoded notes
+        setNotes(savedNotes);
+    }
+}, [])
+
+useEffect(() => {
+    console.log('Notes being saved:', notes);
+    localStorage.setItem(
+        'react-notes-app-data', 
+        JSON.stringify(notes)
+    );
+}, [notes])
+
+  const addNote = (text) => {
+    const date = new Date();
+    const newNote = {
       id: nanoid(),
-      text: "This is first notes!!",
-      date: "01/01/2021"
-    },
-    {
-      id: nanoid(),
-      text: "This is 2 notes!!",
-      date: "01/01/2021"
-    },
-    {
-      id: nanoid(),
-      text: "This is 3 notes!!",
-      date: "01/01/2021"
-    },
-    {
-      id: nanoid(),
-      text: "This is 4 notes!!",
-      date: "01/01/2021"
-    },
-    {
-      id: nanoid(),
-      text: "This is 5 notes!!",
-      date: "01/01/2021"
-    },
-  ]);
-  return <div className="container">
-    <NotesList notes={notes}/>
-  </div>;
+      text: text,
+      date: date.toLocaleDateString()
+    };
+    const newNotes = [...notes, newNote];
+    setNotes(newNotes);
+  }
+
+  const deleteNote = (id) => {
+    const newNotes = notes.filter((note) => note.id !== id);
+    setNotes(newNotes);
+  }
+
+  return (
+    <div className={`${darkMode && "dark-mode"}`}>
+      <div className="container">
+        <Header handleToggleDarkMode={setDarkMode}/>
+        <Search handleSearchNote={setSearchText}/>
+        <NotesList 
+          notes={notes.filter((note) =>
+            note.text.toLowerCase().includes(searchText)
+          )} 
+          handleAddNote={addNote}
+          handleDeleteNote={deleteNote}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- notes-app/src/components/AddNote.js
+++ notes-app/src/components/AddNote.js
@@ -1,10 +1,38 @@
-const AddNote = () => {
+import { useState } from 'react';
+
+const AddNote = ( {handleAddNote} ) => {
+    const [noteText, setNoteText] = useState('');
+
+    //CHARACTER COUNT - display remaining characters
+    const characterLimit = 200;
+
+    const handleChange = (event) => {
+        //CHARACTER COUNT - restrict the number of characters
+        if(characterLimit - event.target.value.length >= 0) {
+            setNoteText(event.target.value);
+        }
+        
+    };
+
+    const handleSaveClick = () => {
+        if(noteText.trim().length > 0) {
+            handleAddNote(noteText);
+            setNoteText('');
+        }
+    };
+
     return (
         <div className="note new">
-            <textarea rows="" cols="10" placeholder="Type to add a note..."></textarea>
+            <textarea
+                rows="8"
+                cols="10" 
+                placeholder="Type to add a note..."
+                value={noteText}
+                onChange={handleChange}
+            ></textarea>
             <div className="note-footer">
-                <small>200 Remaining</small>
-                <button className="save">Save</button>
+                <small>{characterLimit - noteText.length} Remaining</small>
+                <button className="save" onClick={handleSaveClick}>Save</button>
             </div>
         </div>
     );
